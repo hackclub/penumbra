4ac07ab75b8053daa41d8f6bf7dec8c695022da4
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-09-04 21:06:47 -0400

    feat: enhance ShaderEditor with error handling and improve layout in GettingStarted and Home sections

--- src/components/ShaderEditor.svelte
+++ src/components/ShaderEditor.svelte
@@ -2,9 +2,16 @@
   import * as THREE from "three";
   import { glsl } from "codemirror-lang-glsl";
 
-  import { EditorView, basicSetup } from "codemirror";
-  import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
+  import { EditorView } from "codemirror";
+  import { drawSelection, highlightActiveLine, dropCursor, highlightSpecialChars, keymap, rectangularSelection, crosshairCursor, lineNumbers, highlightActiveLineGutter } from "@codemirror/view";
+  import { EditorState } from "@codemirror/state";
+  import { defaultHighlightStyle, syntaxHighlighting, indentOnInput, bracketMatching, HighlightStyle } from "@codemirror/language";
+  import { defaultKeymap, history, historyKeymap, indentWithTab } from "@codemirror/commands";
+  import { searchKeymap, highlightSelectionMatches } from "@codemirror/search";
+  import { autocompletion, closeBrackets, closeBracketsKeymap } from "@codemirror/autocomplete";
   import { tags } from "@lezer/highlight";
+
+  import { onMount } from "svelte";
 
   let { initialCode }: {
     initialCode: string
@@ -12,14 +19,16 @@
 
   let editorElement: HTMLElement;
   let previewElement: HTMLCanvasElement;
+  let shaderErrors: string[] = $state([]);
 
-  $effect(() => {
+  onMount(() => {
     function buildShader(code: string) {
       return /*glsl*/`
         #include <common>
         
         uniform vec3 iResolution;
         
+        // ${Math.random()}
         ${code}
 
         void main() {
@@ -30,6 +39,21 @@
 
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: previewElement });
     renderer.autoClear = false;
+    renderer.debug.onShaderError = (gl, program, glVertShader, glFragShader) => {
+      const errors = gl.getShaderInfoLog(glFragShader)!.trim();
+      
+      shaderErrors = errors.split("\n")
+        .filter(x => !x.includes("\0"))
+        .map(x => {
+          const matches = /^ERROR: ([0-9]+):([0-9]+): (.+)/.exec(x);
+          if (!matches)
+            return x;
+
+          return `ERROR at line ${parseInt(matches[2]) - 137}: ${matches[3]}`;
+        });
+        
+      console.error(`Shader compilation failed!\n\n${errors}`);
+    };
 
     const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
     const scene = new THREE.Scene();
@@ -44,6 +68,7 @@
     scene.add(mesh);
     
     function refresh(code: string) {
+      shaderErrors = [];
       mesh.material = new THREE.ShaderMaterial({
         uniforms,
         fragmentShader: buildShader(code)
@@ -54,11 +79,31 @@
 
     refresh(initialCode);
 
-    const editor = new EditorView({
+    new EditorView({
       parent: editorElement,
       doc: initialCode,
       extensions: [
-        basicSetup,
+        lineNumbers(),
+        highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        history(),
+        drawSelection(),
+        dropCursor(),
+        EditorState.allowMultipleSelections.of(true),
+        indentOnInput(),
+        bracketMatching(),
+        closeBrackets(),
+        autocompletion(),
+        rectangularSelection(),
+        highlightActiveLine(),
+        highlightSelectionMatches(),
+        keymap.of([
+          ...closeBracketsKeymap,
+          ...defaultKeymap,
+          ...searchKeymap,
+          ...historyKeymap,
+          indentWithTab
+        ]),
         glsl(),
         syntaxHighlighting(HighlightStyle.define([
           { tag: tags.comment, class: "code-tag-comment" },
@@ -76,43 +121,77 @@
 
           refresh(ev.state.doc.toString().trim());
         })
-      ],
+      ]
     });
   });
 </script>
 
 <div class="shader-editor">
-  <div bind:this={editorElement} class="editor"></div>
-  <canvas bind:this={previewElement} class="preview" width="600" height="200"></canvas>
+  <div class="editor-container">
+    <div bind:this={editorElement} class="editor"></div>
+    <div class="errors">
+      {#each shaderErrors as error}
+        <div class="error">{error}</div>
+      {/each}
+    </div>
+  </div>
+
+  <canvas bind:this={previewElement} class="preview" width="300" height="200"></canvas>
 </div>
 
 <style lang="scss">
   .shader-editor {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     gap: 1em;
 
-    > .editor {
-      color: #FFFFFF;
-      background: #0009;
+    > .editor-container {
+      width: 100%;
 
-      :global {
-        * {
-          font-family: Nothing, monospace !important;
+      > .errors {
+        display: flex;
+        flex-direction: column;
+        background: #fff;
+        color: #000;
+        
+        > * {
+          width: 100%;
+          padding: 0 1em;
+
+          &:first-child { padding-top: 1em; }
+          &:last-child { padding-bottom: 1em; }
         }
-
-        .cm-editor .cm-cursor {
-          border-left: 2px solid white;
-          transform: translateX(-2px);
-        }
-
-        .code-tag-comment { color: #A4A4A4; }
-        .code-tag-keyword { color: #FFA352; }
       }
+
+      > .editor {
+        color: #FFFFFF;
+        background: #0009;
+        border: 1px solid #fff;
+        width: 100%;
+
+        :global {
+          * {
+            font-family: Nothing, monospace !important;
+          }
+
+          .cm-content { padding: .5em; }
+          .cm-gutter {
+            padding-left: .5em;
+          }
+
+          .cm-editor .cm-cursor {
+            border-left: 2px solid white;
+            transform: translateX(2px);
+          }
+
+          .code-tag-comment { color: #A4A4A4; }
+          .code-tag-keyword { color: #FFA352; }
+        }
+    }
     }
 
     > .preview {
-      border: 2px solid white;
+      border: 1px solid white;
       background: black;
     }
   }
