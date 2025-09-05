6291f265855e974915eb2860f5a0ee11ad2a73af
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-09-05 17:28:45 -0400

    feat: integrate playformCompress into Astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://penumbra.hackclub.com",
   scopedStyleStrategy: "class",
   devToolbar: { enabled: false },
-  integrations: [svelte()],
-  vite: {
-    optimizeDeps: { include: ["monaco-editor"] },
-    css: {
-      preprocessorOptions: {}
-    }
-  }
+  integrations: [svelte(), playformCompress()]
 });
